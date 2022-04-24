7846f220623a85464bc72f90f915d04b36135b4b
Ronald Tischliar <rtischliar@MacBook-Pro-von-Ronald.fritz.box>
2022-04-24 22:08:36 +0200

    three number sum

--- goproblems/sumnumbers/sumnumbers.go
+++ goproblems/sumnumbers/sumnumbers.go
@@ -1,5 +1,7 @@
 package sumnumbers
 
+import "sort"
+
 func TwoNumberSum(array []int, target int) []int {
 	nums := map[int]bool{}
 	for _, num := range array {
@@ -10,4 +12,25 @@ func TwoNumberSum(array []int, target int) []int {
 		nums[num] = true
 	}
 	return []int{}
+}
+
+func ThreeNumberSum(array []int, target int) [][]int {
+	sort.Ints(array)
+	result := [][]int{}
+	for i := 0; i < len(array)-2; i++ {
+		left, right := i+1, len(array)-1
+		for left < right {
+			currentSum := array[i] + array[left] + array[right]
+			if currentSum == target {
+				result = append(result, []int{array[i], array[left], array[right]})
+				left += 1
+				right -= 1
+			} else if currentSum < target {
+				left += 1
+			} else if currentSum > target {
+				right -= 1
+			}
+		}
+	}
+	return result
 }
--- goproblems/sumnumbers/sumnumbers_test.go
+++ goproblems/sumnumbers/sumnumbers_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/ronaldTischliar/examples/goproblems/sumnumbers"
 )
 
-func Test(t *testing.T) {
+func Test2NumberSum(t *testing.T) {
 	t.Run("ok result", func(t *testing.T) {
 		expected := []int{11, -1}
 		result := sumnumbers.TwoNumberSum([]int{3, 5, -4, 8, 11, 1, -1, 6}, 10)
@@ -19,6 +19,24 @@ func Test(t *testing.T) {
 	t.Run("no sum", func(t *testing.T) {
 		expected := []int{}
 		result := sumnumbers.TwoNumberSum([]int{3, 5, -4, 8, 11, 1, -1, 6}, 20)
+		if !reflect.DeepEqual(expected, result) {
+			t.Errorf("Expected %v got %v", expected, result)
+		}
+	})
+}
+
+func Test3NumberSum(t *testing.T) {
+	t.Run("ok result", func(t *testing.T) {
+		expected := [][]int{{-8, 2, 6}, {-8, 3, 5}, {-6, 1, 5}}
+		result := sumnumbers.ThreeNumberSum([]int{12, 3, 1, 2, -6, 5, -8, 6}, 0)
+		if !reflect.DeepEqual(expected, result) {
+			t.Errorf("Expected %v got %v", expected, result)
+		}
+	})
+
+	t.Run("no sum", func(t *testing.T) {
+		expected := [][]int{}
+		result := sumnumbers.ThreeNumberSum([]int{12, 3, 1, 2, -6, 5, -8, 6}, 30)
 		if !reflect.DeepEqual(expected, result) {
 			t.Errorf("Expected %v got %v", expected, result)
 		}
