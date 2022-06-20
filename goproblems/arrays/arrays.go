eddb9c30d6b0b845fa962520c48696d90bcd6807
Ronald Tischliar <rtischliar@proton.me>
2022-06-20 09:57:21 +0200

    sorted squared array

--- goproblems/arrays/arrays.go
+++ goproblems/arrays/arrays.go
@@ -12,3 +12,29 @@ func IsValidSubsequence(array []int, sequence []int) bool {
 	}
 	return seqIdx == len(sequence)
 }
+
+// O(n) time O(n) space
+// input array is in ascemding order
+func SortedSquaredArray(array []int) []int {
+	result := make([]int, len(array))
+	smallerValueIdx := 0
+	largerValueIdx := len(array) - 1
+	for idx := len(array) - 1; idx >= 0; idx-- {
+		smallerValue := array[smallerValueIdx]
+		largerValue := array[largerValueIdx]
+		if abs(smallerValue) > abs(largerValue) {
+			result[idx] = smallerValue * smallerValue
+			smallerValueIdx += 1
+		} else {
+			result[idx] = largerValue * largerValue
+			largerValueIdx -= 1
+		}
+	}
+	return result
+}
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
--- goproblems/arrays/arrays_test.go
+++ goproblems/arrays/arrays_test.go
@@ -1,6 +1,7 @@
 package arrays_test
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/ronaldTischliar/examples/goproblems/arrays"
@@ -26,4 +27,17 @@ func TestSequence(t *testing.T) {
 			t.Errorf("got %v want %v", isSequence, false)
 		}
 	})
+
+}
+
+func TestSortedSquaredArray(t *testing.T) {
+	t.Run("sequence", func(t *testing.T) {
+		input := []int{-6, -3, 1, 2, 5, 8, 9}
+		expected := []int{1, 4, 9, 25, 36, 64, 81}
+		result := arrays.SortedSquaredArray(input)
+		if !reflect.DeepEqual(expected, result) {
+			t.Errorf("Expected %v got %v", expected, result)
+		}
+	})
+
 }
